7da790c2752c63dca01a9c9b2ff948c167f95b76
Adam <adam5596@stud.kea.dk>
2022-11-02 22:29:32 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import Navbar from './Components/Navbar';
 import { Container } from '@mui/system';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import { history } from './services/history';
 import HomePage from './Components/HomePage';
 import ListArtistPage from './Components/ListArtistPage';
 import ArtistPage from './Components/ArtistDetailPage/ArtistPage';
 import ErrorPage from './Components/ErrorPage';
 import LoginPage from './Components/LoginPage/';
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter hisotry={history}>
       <div className="App">
         <Navbar />
         <Container maxWidth="md">
           <Routes>
-            <Route path="/error" element={<ErrorPage />} />
             <Route path="/" element={<HomePage />} />
+            <Route path="/error" element={<ErrorPage />} />
             <Route path="/artists" element={<ListArtistPage />} />
             <Route path='/artist/:artistName' element={<ArtistPage />} />
             <Route path='/login/' element={<LoginPage />} />
